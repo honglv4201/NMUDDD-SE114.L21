3b7c73c0ea1197d47027dd55e17fbbc922e75bbd
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2021-07-07 00:55:15 +0700

    sheet bar + private post setting + user find+...

--- app/src/main/java/com/lamhong/mybook/MainActivity.kt
+++ app/src/main/java/com/lamhong/mybook/MainActivity.kt
@@ -3,6 +3,9 @@ package com.lamhong.mybook
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.gms.tasks.OnCompleteListener
@@ -84,6 +87,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
         MyFirebaseMessagingService.sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            moveFragment(zHome())
+        }, 3500)
+        Handler(Looper.getMainLooper()).postDelayed({
+            moveFragment(zHome())
+        }, 2000)
+        //moveFragment(zHome())
+
     }
 
 
